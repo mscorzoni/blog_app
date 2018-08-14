9e74a85dad3a0e665acf7fc8e6d26ff1034db5fd
Marcos Scorzoni <mlessa@gmail.com>
2018-08-14 11:46:47 +0100

    add user references to articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_13_171745) do
+ActiveRecord::Schema.define(version: 2018_08_14_104258) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
